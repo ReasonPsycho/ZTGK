75c48389875f6f41b4c2c4679395abef68c236f6
ReasonPsycho <54778479+ReasonPsycho@users.noreply.github.com>
2024-03-23 17:08:26 +0100

    Add PBRPrimitives class and improve texture loading
    A new class, PBRPrimitives, has been added for rendering cube and quad primitives. The Model and Material classes are now using PBRPrimitives instead of the normal Primitives class. Moreover, improvements have been made to the loading process of materials' textures, including efficient handling of missing textures by assigning default color values.

--- src/ECS/Render/ModelLoading/Mesh.cpp
+++ src/ECS/Render/ModelLoading/Mesh.cpp
@@ -55,7 +55,7 @@ void Mesh::setupMesh() {
     // vertex Positions
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) 0);
-    // vertex normals
+    // vertex normals 
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, Normal));
     // vertex texture coords
