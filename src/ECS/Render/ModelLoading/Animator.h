c13a8bccc7335bb198ed1bc53c4f24f82b290ca5
ReasonPsycho <54778479+ReasonPsycho@users.noreply.github.com>
2024-06-10 01:43:44 +0200

    To large for AI XD

--- src/ECS/Render/ModelLoading/Animator.h
+++ src/ECS/Render/ModelLoading/Animator.h
@@ -20,11 +20,12 @@ public:
     void CalculateBoneTransform(const AssimpNodeData* node, glm::mat4 parentTransform);
 
     std::vector<glm::mat4> GetFinalBoneMatrices();
+    
+    Animation* m_CurrentAnimation;
 
+    float m_CurrentTime;
 private:
     std::vector<glm::mat4> m_FinalBoneMatrices;
-    Animation* m_CurrentAnimation;
-    float m_CurrentTime;
     float m_DeltaTime;
 };
 
