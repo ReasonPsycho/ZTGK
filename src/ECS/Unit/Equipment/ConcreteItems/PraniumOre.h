8ca714a4ffd429cc9eea35e78b47052abde773a3
IgorKusidel <igor.kusidel@gmail.com>
2024-06-20 21:52:35 +0200

    Minor changes

--- src/ECS/Unit/Equipment/ConcreteItems/PraniumOre.h
+++ src/ECS/Unit/Equipment/ConcreteItems/PraniumOre.h
@@ -10,7 +10,7 @@ class PraniumOre : public Item{
 public:
     PraniumOre() : Item("Pranium Ore", "Magical, rare, valuable! Let's get cleaning!", false, ItemStats(1, 1, GridRange(1, 0)), false) {
         model = ztgk::game::praniumModel;
-        icon_path = "res/textures/puni.png";
+        icon_path = "res/textures/pranium_ore.png";
     }
 
 };
